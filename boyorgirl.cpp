0c2ddf21657368e1a4856b25aa3088c1a0087be1
Anushka Mishra <57721771+AnushkaMishra29@users.noreply.github.com>
2020-12-23 00:18:14 +0530

    day-4

--- boyorgirl.cpp
+++ boyorgirl.cpp
@@ -1,18 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-	char arr[101];
-	int count=0;
-	for(int i=0;i<101;i++){
-		cin>>arr[i];
+	unordered_map<char,int>m1,m2;
+	char arr[200],brr[200];
+	string a,b;
+	cin>>a;
+	cin>>b;
+	string c;
+	cin>>c;
+	int p=c.length();
+	int m=a.length();
+	int n=b.length();
+	for(int i=0;i<m;i++){
+		arr[i]=a[i];
 	}
-	int l=strlen(arr);
-	sort(arr,arr+l);
-	for(int i=0;i<l-1;i++){
-		if(arr[i]!=arr[i+1]){count++;}
+	int j=0;
+	for(int i=m;i<(m+n);i++){
+		arr[i]=b[j];
+		j++;
 	}
-	if(count%2==0){cout<<"CHAT WITH HER"<<endl;}
-	else{cout<<"IGNORE HIM"<<endl;}
-	
+	for(int i=0;i<(m+n);i++){
+		m1[arr[i]]++;
+	}
+	for(int i=0;i<p;i++){
+		brr[i]=c[i];
+	}
+	for(int i=0;i<p;i++){
+		m2[brr[i]]++;
+	}
+	if(m1==m2){cout<<"YES"<<endl;}
+	else cout<<"NO"<<endl;
 	return 0;
+
+
 }
