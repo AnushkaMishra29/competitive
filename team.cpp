640abf864a731750687e75432f5b7a00784a6036
Anushka Mishra <57721771+AnushkaMishra29@users.noreply.github.com>
2020-12-25 11:10:56 +0530

    day-7

---
+++ team.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int n;cin>>n;
+	int arr[n][3];
+	for(int i=0;i<n;i++){
+		for(int j=0;j<3;j++){
+			cin>>arr[i][j];
+		}
+	}
+	int count=0,num=0;
+	for(int i=0;i<n;i++){
+		for(int j=0;j<3;j++){
+			if(arr[i][j]==1){
+				count++;}
+			
+		}
+		if(count>1){num++;}
+		count=0;
+	}
+	cout<<num<<endl;
+	return 0;
+}
